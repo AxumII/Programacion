2f8da4497336ca3196593732f5d8e0bdcfad2600
AxumII <axumprofile@gmail.com>
2023-08-14 20:27:43 -0500

    Crud usuarios
    primera parte funcional del crud de usuarios

--- Sistema de Finanzas Desktop II/createDB.py
+++ Sistema de Finanzas Desktop II/createDB.py
@@ -4,20 +4,38 @@ def create_product_table():
     conexion = sqlite3.connect("database.db")
     cursor = conexion.cursor()
 
-    cursor.execute('''CREATE TABLE IF NOT EXISTS Product (
+    cursor.execute('''CREATE TABLE IF NOT EXISTS INVENTARIO (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         name VARCHAR(50),
                         unitPrice REAL                            
                     )''')
 
     conexion.commit()
+    
+    
+    cursor.execute('''CREATE TABLE IF NOT EXISTS USUARIO (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        userName VARCHAR(20),
+                        email VARCHAR(30),                        
+                        accessType INTEGER
+                   )''')
+    
+    conexion.commit()
+    
+    
     conexion.close()
+    
+    
+    
+    
+    
 
 if __name__ == "__main__":
     create_product_table()
 
 
-    """cursor.execute('''CREATE TABLE IF NOT EXISTS Product (
+    """cursor.execute('''CREATE TABLE IF NOT EXISTS Inventario (
+        
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         name VARCHAR(50),
                         unitPrice REAL   ,
@@ -29,3 +47,13 @@ if __name__ == "__main__":
                         ejecStat BLOB    
                     )''')
     """
+    
+    
+    """cursor.execute('''CREATE TABLE IF NOT EXISTS USUARIO (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        userName VARCHAR(20),
+                        email VARCHAR(30)
+                        passwordHashed VARCHAR(128),
+                        salt VARCHAR(32),
+                        accessType INTEGER
+                   )''')"""
---
+++ Sistema de Finanzas Desktop II/crudUsuarios.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import sqlite3
+
+# Conectar el DataFrame con la base de datos
+conn = sqlite3.connect('database.db')
+cur = conn.cursor()
+
+# Carga inicial
+query = "SELECT * FROM Usuario"
+UsuariosDf = pd.read_sql_query(query, conn)
+
+def Create(userName, email, accessType):
+    global UsuariosDf    
+    newRow = {'userName': userName, 'email': email, 'accessType': accessType}
+    
+    # Agregar la nueva fila al DataFrame
+    UsuariosDf = pd.concat([UsuariosDf, pd.DataFrame(newRow, index=[0])], ignore_index=True)
+    
+    # Insertar la nueva fila en la tabla
+    query = f"INSERT INTO Usuario (userName, email, accessType) VALUES ('{newRow['userName']}', '{newRow['email']}', '{newRow['accessType']}')"
+    cur.execute(query)
+    conn.commit()
+
+def ReadAll():
+    query = "SELECT * FROM Usuario"
+    UsuariosDf = pd.read_sql_query(query, conn)
+    print('Muestra la tabla de Usuarios\n')
+    print(UsuariosDf)
+
+def ReadSingle(rowId):
+    query = f"SELECT * FROM Usuario WHERE id = {rowId}"
+    singleRow = pd.read_sql_query(query, conn)
+    if len(singleRow) == 0:
+        print(f"No se encontró ningún usuario con ID {rowId}.")
+    else:
+        print('Muestra un único usuario\n')
+        print(singleRow)
+
+def Update(pos, userName, email,accessType):
+    global UsuariosDf
+
+    # Extrae los datos actuales del usuario indicado
+    oldRow = UsuariosDf.iloc[pos].to_dict()
+    
+    # Crea un diccionario con los nuevos valores proporcionados
+    newRow = {'userName': userName, 'email': email, 'accessType': accessType}
+    
+    print("Usuario antiguo:")
+    print(oldRow, "\n")
+    
+    print("Usuario nuevo:")
+    print(newRow, "\n")
+    
+    # Actualizar el DataFrame
+    UsuariosDf.loc[pos] = newRow
+    
+    # Actualizar el usuario en la tabla
+    query = f"UPDATE Usuario SET userName = '{newRow['userName']}', email = '{newRow['email']}', accessType = '{newRow['accessType']}' WHERE id = {pos + 1}"
+    cur.execute(query)
+    conn.commit()
+
+def Delete(rowId):
+    global UsuariosDf
+
+    # Eliminar el usuario del DataFrame
+    UsuariosDf = UsuariosDf[UsuariosDf.index != rowId - 1].reset_index(drop=True)
+    
+    # Eliminar el usuario de la tabla en la base de datos
+    query = f"DELETE FROM Usuario WHERE id = {rowId}"
+    cur.execute(query)
+    conn.commit()
+
+    print(f"Usuario con ID {rowId} eliminado correctamente.")
+
+# Ejemplos de uso
+ReadAll()
+Create('usuario1', 'usuario1@example.com',2)
+Create('usuario2', 'usuario2@example.com',1)
+ReadAll()
+Update(1, 'nuevo_usuario', 'nuevo@example.com',0)
+ReadAll()
+Delete(2)
+ReadAll()
+ReadSingle(2)
+
+# Cerrar la conexión con la base de datos
+conn.close()
