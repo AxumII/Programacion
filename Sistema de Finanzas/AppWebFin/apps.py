e8c9cc0d47a6395af58f6bac62da77d83ab17f75
AxumII <141262236+AxumII@users.noreply.github.com>
2024-07-29 12:33:23 -0500

    a
    a

--- Sistema de Finanzas/AppWebFin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppwebfinConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AppWebFin'
--- Sistema de Finanzas/AppWebFin/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.tablaProductos, name='prueba'),
-    path('guardado/', views.guardarProductos, name="guardado"), 
-    # Otras URLS de tu aplicación, si las tienes.
-]
