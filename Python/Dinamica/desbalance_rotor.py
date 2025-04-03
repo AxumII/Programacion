c347a2a0f07c724a3c17e16829a8759aeb8e6deb
AxumII <141262236+AxumII@users.noreply.github.com>
2025-04-03 11:12:15 -0500

    xd
    xd

--- Python/Dinamica/desbalance_rotor.py
+++ Python/Dinamica/desbalance_rotor.py
@@ -1,92 +1,70 @@
 import numpy as np
 import sympy as sp
 
-class Desbalance:
-    def __init__(self, k_h=80, k_v = 80, w_t=1, alpha=0, r_ctr_d=1,  L_n=[0.0739, 0.1546, 0.235], L=2, W_peso = -1, L_ctr =1,
-                 D1_ex=[0, 0, 0, 0], D1_m=[0, 0, 0, 0], 
-                 D2_ex=[0, 1, 0, 0], D2_m=[0, 1, 0, 0], 
-                 D3_ex=[0, 0, 0, 0], D3_m=[0, 0, 0, 0], 
-                 theta_init=45):
-        self.theta_init = 0
-        self.w_t = w_t #Frecuencia Forzada (Rev/s)
-        self.alpha = alpha #Aceleracion Angular (requiere torque e Iz)
-        self.L_n = L_n  # Distancias de D1, D2 y D3 respecto a A
-        self.L = L  # Longitud total
-        self.L_ctr = L_ctr #Distancia desde A al centroide total
-        self.r_ctr_d = r_ctr_d  # Radio de círculo centroidal de masas
-        self.k_h = k_h  # Constante de rigidez resortes horizontales
-        self.k_v = k_v  # Constante de rigidez resortes verticales
-        self.W_peso = W_peso
-        # Generar posiciones automáticamente a partir del ángulo inicial
-        self.posiciones = [self.circulo(theta_init), self.circulo(theta_init + 90), 
-                           self.circulo(theta_init - 90), self.circulo(theta_init + 180)]
-
-        # Definir estructuras de D1, D2, y D3 con posiciones generadas
-        self.D1 = np.array([D1_ex, D1_m, self.posiciones], dtype=object)
-        self.D2 = np.array([D2_ex, D2_m, self.posiciones], dtype=object)
-        self.D3 = np.array([D3_ex, D3_m, self.posiciones], dtype=object)
-
-
-    def circulo(self, theta_init):
-        """ Retorna la posición (x, y) de un punto en el círculo. """
-        theta = np.radians(theta_init)
-        x = float(self.r_ctr_d * np.cos(theta))  # Asegurar valores escalares
-        y = float(self.r_ctr_d * np.sin(theta))
-        return (x, y)
-    
-    def pIn_disco(self, D, l):
-        """
-        Calcula Ixz e Iyz para un conjunto de masas y posiciones D (puede ser D1, D2 o D3).
-        Ixz = masa * posición_x * l
-        Iyz = masa * posición_y * l
-        """
-        Ixz, Iyz = 0, 0
+class desb:
+    def __init__(self, k = 40, w_t = 1, alpha = 0, r_ctr_d = 0.038, L = 0.1):
+        self.w_t = w_t
+        self.alpha = alpha
+        self.k = k
+        L_n = []
+        self.L = np.sum(np.array(L_n))
+        self.r_ctr_d= r_ctr_d
         
-        existencia, masa, posiciones = D[0], D[1], D[2]
+        #Propiedades Discos
+        # Matriz de D1
+        self.D1 = np.array([
+            [1, 0, 1, 1],  # Existencia
+            [10.5, 5.0, 8.3, 7.2],  # Masa
+            [self.circulo(0), self.circulo(90), self.circulo(-90), self.circulo(180)]  # Posiciones
+        ])
 
-        # Sumar contribuciones de cada elemento si existe
-        for i in range(len(existencia)):
-            if existencia[i] == 1:  # Si el elemento existe
-                x, y = posiciones[i]  # Extraer x, y de la posición
-                Ixz += masa[i] * x * l
-                Iyz += masa[i] * y * l
+        # Matriz de D2
+        self.D2 = np.array([
+            [0, 1, 1, 0],  # Existencia
+            [6.4, 7.1, 9.2, 3.3],  # Masa
+            [self.circulo(0), self.circulo(90), self.circulo(-90), self.circulo(180)]  # Posiciones
+        ])
+
+        # Matriz de D3
+        self.D3 = np.array([
+            [1, 1, 0, 1],  # Existencia
+            [12.7, 8.8, 4.5, 11.3],  # Masa
+            [self.circulo(0), self.circulo(90), self.circulo(-90), self.circulo(180)]  # Posiciones
+        ])
+
+
+    def circulo(self, theta_init = 0, n = 180):
+        #Permmite mapear un circulo con coordenadas rectngulares y un angulo inicial
+        theta = np.linspace(0 + theta_init, 2*np.pi + theta_init, n)
+        x = self.r_ctr_d*np.cos(theta)
+        y = self.r_ctr_d*np.sin(theta)
+        return x,y
+    
+
+    def pIn_disco(self,D,l):
+        #Se obtiene cada masa con su distancia respectiva y se suman los pdInercia
+        Ixz = 0
+        Iyz = 0
+        for i in range(D.shape[1]):
+            existencia = D[0, i]
+            masa = D[1, i]
+            x, y = D[2, i]
+            Ixz += existencia * masa * x * l
+            Iyz += existencia * masa * y * l
+            
+        return Ixz, Iyz
+    
+    def Inercia(self):
+        Ixz_d1, Iyz_d1 = self.pIn_disco(D = self.D1, l = 0.06)
+        Ixz_d2, Iyz_d2 =  self.pIn_disco(D = self.D2, l = 0.12)
+        Ixz_d3, Iyz_d3 =  self.pIn_disco(D = self.D3, l = 0.18)
+        Ixz = Ixz_d1 + Ixz_d2 + Ixz_d3
+        Iyz = Iyz_d1 + Iyz_d2 + Iyz_d3
+        
         return Ixz, Iyz
 
-    def Inercia(self):
-        # Respecto a A
-        Ixz_d1_A, Iyz_d1_A = self.pIn_disco(self.D1 , self.L_n[0])
-        Ixz_d2_A, Iyz_d2_A = self.pIn_disco(self.D2 , self.L_n[1])
-        Ixz_d3_A, Iyz_d3_A = self.pIn_disco(self.D3 , self.L_n[2])
-        """
-        print("Ixz A Calculado en el disco 1", Ixz_d1_A)
-        print("Iyz A Calculado en el disco 1", Iyz_d1_A)
-        print("Ixz A Calculado en el disco 2", Ixz_d2_A)  
-        print("Iyz A Calculado en el disco 2", Iyz_d2_A)  
-        print("Ixz A Calculado en el disco 3", Ixz_d3_A) 
-        print("Iyz A Calculado en el disco 3", Iyz_d3_A)  
-        """
-        # Respecto a B (Invirtiendo las distancias)
-        Ixz_d1_B, Iyz_d1_B = self.pIn_disco(self.D3 , - self.L +  self.L_n[2])
-        Ixz_d2_B, Iyz_d2_B = self.pIn_disco(self.D2 , - self.L +  self.L_n[1])
-        Ixz_d3_B, Iyz_d3_B = self.pIn_disco(self.D1 , - self.L +  self.L_n[0])
-        """
-        print("Ixz B Calculado en el disco 1", Ixz_d1_B)
-        print("Iyz B Calculado en el disco 1", Iyz_d1_B)
-        print("Ixz B Calculado en el disco 2", Ixz_d2_B)  
-        print("Iyz B Calculado en el disco 2", Iyz_d2_B)  
-        print("Ixz B Calculado en el disco 3", Ixz_d3_B) 
-        print("Iyz B Calculado en el disco 3", Iyz_d3_B)  
-        """
-        # Sumar los valores para cada punto
-        In_A = (Ixz_d1_A + Ixz_d2_A + Ixz_d3_A, Iyz_d1_A + Iyz_d2_A + Iyz_d3_A)
-        In_B = (Ixz_d1_B + Ixz_d2_B + Ixz_d3_B, Iyz_d1_B + Iyz_d2_B + Iyz_d3_B)
 
-    
-
-
-        return In_A, In_B
-
-    def solve_reacciones_din(self):
+    def solve_reacciones_din(self, In_A, In_B, Iz):
         """Se tienen dos apoyos, A y B siendo A el izquierdo y B el derecho
         Ya estan en equilibrio estatico, por lo que solo interesan las fuerzas dinamicas
         El eje Z es en el que rota el eje
@@ -94,27 +72,22 @@ class Desbalance:
         Se tienen dos puntos de referencia, A para calcular las reacciones en B , B para calcular las reacciones en A
         #En A, L es positivo, pero respecto a B, L es negativo ya que apunta hacia A
         """
-        In_A, In_B = self.Inercia()
-
-        # Sistema de Referencia en A
+        #Sistema de Referencia en A
         Ixz_A, Iyz_A = In_A
-        By = (((Ixz_A * self.alpha ) - (Iyz_A * self.w_t ** 2 )) / self.L)
-        Bx = (((-Iyz_A * self.alpha ) + (-Ixz_A * self.w_t ** 2 )) / self.L)
+        By =   -1*(((-1*Ixz_A * self.alpha ) + (Iyz_A * self.w_t**2 )) / self.L)
+        Bx =    (((-1*Iyz_A * self.alpha ) + (-1*Ixz_A * self.w_t**2 )) / self.L)
 
-        # Sistema de Referencia en B
-        Ixz_B, Iyz_B = In_B 
-        Ay = (((-Ixz_B * self.alpha ) + (Iyz_B * self.w_t ** 2 )) / self.L)
-        Ax = -(((-Iyz_B * self.alpha ) + (-Ixz_B * self.w_t ** 2 )) / self.L)
-
-        return [Ax, Ay], [Bx, By]
-    
-    def solver_reacciones_est(self):
+        #Sistema de Referencia en B
         
-        M = np.array([[1,1], [0,self.L]])
-        S = np.array([-self.W_peso,-self.W_peso*self.L_ctr ])
-        return  np.linalg.solve(M,S)
+        Ixz_B, Iyz_B = In_B 
+        Ay =    (((-1*Ixz_B * self.alpha ) + (Iyz_B * self.w_t**2 )) / self.L)
+        Ax =    -1*(((-1*Iyz_B * self.alpha ) + (-1*Ixz_B * self.w_t**2 )) / self.L)
 
-    def distancias_desbalance(self):
+        B = [Bx,By]
+        A = [Ax,Ay]
+        return A,B
+    
+    def distancias_desbalance(self, Bx, By, Ax, Ay):
         """
         Aca ya se conocen las reacciones dinamicas y estaticas, por lo que se procede a calcular la desviacion
         en cada apoyo en cada eje
@@ -125,84 +98,8 @@ class Desbalance:
         En el eje y, actuan las reacciones estaticas verticuales y las dinamicas verticales. Las dinamicas ya las tenemos por calculo, mas las estaticas
         tienen en cuenta el peso de todo el sistema repartido en ambos apoyos. Esto implica que en estatico los resortes ya presentan una deformacion inicial de modo que toca tenerla en cuenta
         """
-        [Ax_D, Ay_D], [Bx_D, By_D] = self.solve_reacciones_din()
-        Ay_E, By_E = self.solver_reacciones_est()
-        Ax = Ax_D 
-        Ay = Ay_D + Ay_E
-        Bx = Bx_D
-        By = By_D + By_E
 
-        X_a = 2 * Ax / self.k_h
-        Y_a = 2 * Ay / self.k_v
-        X_b = 2 * Bx / self.k_h
-        Y_b = 2 * By / self.k_v
-        
-        return [X_a, Y_a], [X_b, Y_b]
-
-    def cal_k(self):
-        #se usa para saber cuanto se debe elongar el resorte con un K dado con una masa en mm
-        #W = k*y
-        #y = w/k
-        m = 0.20
-        return 9.81*m /self.k_h  *1000  , 9.81*m /self.k_v  *1000 
-
-
-"""
-
-#Ejemplos
-default = Desbalance()
-print(default.Inercia())
-print(default.solve_reacciones_din())
-#################################################################################################################
-libro = Desbalance( w_t=125.7, alpha=3000, r_ctr_d=0.05, L_n=[0.15, 0.3, 0.45], L=0.6, theta_init=0, 
-                 D1_ex=[0, 0, 0, 1], D1_m=[0, 0, 0, 0.3], 
-                 D2_ex=[0, 1, 0, 0], D2_m=[0, 0.3, 0, 0], 
-                 D3_ex=[0, 0, 0, 0], D3_m=[0, 0, 0, 0])
-print("\nInercia en A y B (custom):", libro.Inercia())
-print("Reacciones dinámicas en A y B (custom):", libro.solve_reacciones_din())
-"""
-
-"""
-k_h = 500
-k_v = 700
-m_tornillo = 8/1000 #Gramos
-L_t = 0.31
-masa_t = (3*(36)) + (0.18) + ((15) + (15)) +((6)* (m_tornillo))  #3discos + Eje + 2 rodamientos + n Masas  EN GRAMOS
-g = -9.81
-W_peso_t = (masa_t*g)/1000
-w_t  = (500)*(np.pi/30) #rpm
-
-
-print("masa total gramos", masa_t)
-print("peso total N", W_peso_t)
-rotor = Desbalance(k_h=k_h, k_v=k_v ,w_t=w_t, alpha=0, r_ctr_d=0.035, L_n=[0.0739, 0.1546, 0.235], L= L_t, theta_init=0, L_ctr= L_t / 2, W_peso = W_peso_t,
-                    D1_ex=[0, 1, 1, 1], D1_m=[0, m_tornillo, m_tornillo, m_tornillo], 
-                    D2_ex=[0, 1, 0, 0], D2_m=[0, m_tornillo, 0, 0], 
-                    D3_ex=[1, 0, 1, 0], D3_m=[m_tornillo, 0, m_tornillo, 0])
-
-    # Calcular la inercia en A y B
-inercia_rotor = rotor.Inercia()
-print("\nInercia en A y B (rotor):", inercia_rotor)
-
-    # Calcular las reacciones dinámicas en A y B
-reacciones_din_rotor = rotor.solve_reacciones_din()
-print("Reacciones dinámicas en A y B (rotor):", reacciones_din_rotor)
-
-    # Calcular las reacciones Estaticas en A y B
-reacciones_est_rotor = rotor.solver_reacciones_est()
-print("Reacciones Estaticas en A y B (rotor):", reacciones_est_rotor)
-
-    # Calcular las distancias de desbalance
-desbalance_rotor = rotor.distancias_desbalance()
-print("Distancias de desbalance en A y B (rotor): (x,y), Metros", desbalance_rotor)
-desbalance_rotor * 1000
-print("Distancias de desbalance en A y B (rotor): (x,y), Milímetros", [[x * 1000 for x in desbalance_rotor[0]], [y * 1000 for y in desbalance_rotor[1]]])
-
-
-    #Distancia de elongacion de resorte para fabricacion
-elongacion = rotor.cal_k()
-print("La elongacion en mm es", elongacion)
-
-
-
-      """
+        X_b = 2*Bx / self.k
+        Y_b = 2*By / self.k
+        X_a = 2*Ax / self.k
+        Y_a = 2*Ay / self.k
