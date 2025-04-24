e7934b75e684adb1407abd6e0eb6b02c140e96ac
AxumII <axumprofile@gmail.com>
2025-04-23 21:54:47 -0500

    galga
    galg

---
+++ Python/SYS/numerico.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def Calculo():
+    def derivadas():
+        pass
+
+    def integrales():
+        pass
+
+    def edo():
+        pass
+
+def S_Ec():
+    def sistemanxn():
+        pass
+
+    def roots():
+        pass
+
+def A_S():
+    def fourier():
+        pass
+    
