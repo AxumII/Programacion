33e43464217b5dfdee3560e412638bf6a614a97b
AxumII <axumprofile@gmail.com>
2025-05-08 23:34:04 -0500

    galga 7
    ahora si galga 7 con model ode galga y de pie de rey a fulkl con implementacion

---
+++ Python/Galga/ie_one_s.py
@@ -0,0 +1,36 @@
+import scipy.stats as st
+import numpy as np
+
+
+class Estimador:
+    def __init__(self, muestra, alpha):
+        self.muestra = muestra
+        self.alpha = alpha
+        self.n = muestra.shape[0]  # Tamaño de la muestra
+
+    def est_norm(self):  # Estimadores de distribución normal
+        X_m = np.mean(self.muestra)               # Media muestral
+        Sn2_m = np.var(self.muestra, ddof=1)      # Varianza muestral (ddof=1)
+        #Usa el estimador sesgado, dado a que es el MLE, mas no usa el insesgado ya que se asume 
+        #que se usaran muestras grandes
+        return X_m, Sn2_m
+
+    def ic_norm(self):
+        X_m, Sn2_m = self.est_norm()
+
+        # IC para la media 
+        margen = st.t.ppf(1 - self.alpha / 2, self.n - 1) * np.sqrt(Sn2_m / self.n)
+        X_m_min = X_m - margen
+        X_m_max = X_m + margen
+
+        # IC para la varianza 
+        chi_l = st.chi2.ppf(self.alpha / 2, df=self.n - 1)  
+        chi_u = st.chi2.ppf(1 - self.alpha / 2, df=self.n - 1)  
+
+        var_min = ((self.n - 1) * Sn2_m) / chi_u  
+        var_max = ((self.n - 1) * Sn2_m) / chi_l  
+
+        return {
+            "media": (X_m_min, X_m_max),
+            "varianza": (var_min, var_max)
+        }
--- Python/Galga/unc.py
+++ Python/Galga/unc.py
@@ -1,69 +1,44 @@
 import numpy as np
 import pandas as pd
-import inspect
 import sympy as sp
 class Type_A:
     def __init__(self, muestra: np.ndarray, name=None):
         self.muestra = muestra
         self.name = name
-        self.columns = ["Name", "Uncertainty", "Sensitivity", "Degrees_of_Freedom"]
-        self.Sn_m = self.des_est()
-        
+        self.columns = ["Name", "Uncertainty", "Sensitivity"]
+        self.df_unc =  pd.DataFrame(columns=self.columns, dtype=float)
 
-    def des_est(self):
+    def des_est(self, sensitivity=1):
         label = "Statical" if not self.name else f"Statical of {self.name}"
-        uncertainty = np.sqrt(np.var(self.muestra, ddof=1))
-        sensitivity = 1
+        uncertainty = np.std(self.muestra, ddof=1)
+        self.df_unc.loc[len(self.df_unc)] = [label, uncertainty, sensitivity]
 
-        df_out = pd.DataFrame(columns=self.columns)
-        df_out.loc[0] = [label, uncertainty, sensitivity]
 
-        return df_out
     
 class Type_B:
-    def __init__(self, resol = None, calib = None, name=None, f_c_calib=3):
-        self.name = name
+    def __init__(self, resol=None, calib=None, name=None, f_c_calib=3):
         self.resol = resol
         self.calib = calib
+        self.name = name
         self.f_c_calib = f_c_calib
         self.columns = ["Name", "Uncertainty", "Sensitivity"]
         self.df_unc = pd.DataFrame(columns=self.columns)
-        
-    def unc_resol(self, sensitivity = None):
-        if self.resol is not None:
-            sensitivity = sensitivity if sensitivity is not None else 1
-            label = "Resolution" if not self.name else f"Resolution of {self.name}"
-            return label, (self.resol / np.sqrt(12)), 1
 
-    def unc_calib(self, sensitivity=None):
-        sensitivity = sensitivity if sensitivity is not None else 1
+    def coef_sens(self, i_var, equation, num_values):
+        return float(sp.diff(equation, i_var).evalf(subs=num_values))
+
+    def unc_resol(self, sensitivity=1):
+        if self.resol is not None:
+            label = "Resolution" if not self.name else f"Resolution of {self.name}"
+            uncertainty = self.resol / np.sqrt(12)
+            self.df_unc.loc[len(self.df_unc)] = [label, uncertainty, sensitivity]
+
+    def unc_calib(self, sensitivity=1):
         if self.calib is not None:
             label = "Calibration" if not self.name else f"Calibration of {self.name}"
             uncertainty = self.calib / self.f_c_calib
-            return label, uncertainty, sensitivity
+            self.df_unc.loc[len(self.df_unc)] = [label, uncertainty, sensitivity]
 
-    
-    def coef_sens(self,i_var, equation, num_values):
-        diff_equation = sp.diff(equation, i_var)
-        return diff_equation.evalf(subs = num_values)
-
-    def add(self):
-        for nombre, metodo in inspect.getmembers(self, predicate=inspect.ismethod):
-            if nombre.startswith("unc_"):
-                resultado = metodo()
-                if resultado is None:
-                    continue
-                elif isinstance(resultado, tuple) and len(resultado) == 3:
-                    self.df_unc.loc[len(self.df_unc)] = resultado
-                elif isinstance(resultado, pd.DataFrame):
-                    self.df_unc = pd.concat([self.df_unc, resultado], ignore_index=True)
-
-class Expanded:
-    def __init__(self):
-        pass
-    
-    def unc_combined(self):
-        pass
-        #Entra un dataframe
-    def unc_expand(self):
-        pass
+    def add(self, sensitivity=1):
+        self.unc_resol(sensitivity=sensitivity)
+        self.unc_calib(sensitivity=sensitivity)
