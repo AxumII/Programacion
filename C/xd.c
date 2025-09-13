5529833b7522c60d8566d0428e0c290303f74949
AxumII <141262236+AxumII@users.noreply.github.com>
2025-09-13 14:49:20 -0500

    aaa
    a

---
+++ C/xd.c
@@ -0,0 +1,29 @@
+
+
+
+int main(){
+    //Metodo ASCII
+    int asciitoBin(char buchstabe){
+        switch(buchstabe){
+
+            case 'a':
+            return 01100001;
+
+            case 'b':
+            return 01100010;
+        }
+    }
+
+
+    //Metodo String to Bin
+    int stringtoBin(char string[]){
+
+        for(i; i < ; i++)
+            bstb = string[i];
+            return asciitoBin(bstb);
+    }
+
+    //Metodo Escritura en LCD dado un char
+    int writeLCD()
+
+}
