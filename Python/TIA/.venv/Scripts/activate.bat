caeb1e8ef323d2a57269c0a81944496e94da8a37
AxumII <axumprofile@gmail.edu.co>
2026-05-06 14:30:54 -0500

    xd
    xd tia 5

--- Python/TIA/.venv/Scripts/activate.bat
+++ Python/TIA/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\GitHub\Programacion\Python\TIA\.venv"
+set "VIRTUAL_ENV=C:\Github\Programacion\Python\TIA\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
