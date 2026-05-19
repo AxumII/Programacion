355df98dfe9628259c8858d0ed98f792fe76fa05
AxumII <axumprofile@gmail.edu.co>
2026-05-19 17:03:24 -0500

    2gof
    se añadio el caso de 2gof en dinamica de modo que se calcula la cinetica basica del brazo

--- Python/Dinamica/Brazo/exekinetic3d.py
+++ Python/Dinamica/Brazo/exekinetic3d.py
@@ -97,6 +97,49 @@ def caso4_pol_gof():
     arm_6dof.analyze_pose([0, 0, 0, 0, 0, 0])
 
 
+def caso2_pol_gof():
+
+    m_jgy370 = 180/1000
+
+    m_load = 20 /1000 
+    
+    m_polea = 15/1000
+    
+    m_aluminio = 60/1000
+    
+    m_PLA = 20/1000
+
+    robot_def = [
+        {'axis': 'z', 'offset': [0.15, 0, 0],       'm_servo': m_jgy370, 'm_link': 0.1, 'com_link': [0, 0, 0.05], 't_rated':1.0 },
+        {'axis': 'y', 'offset': [0.2, 0, 0],    'm_servo': m_jgy370 * 2, 'm_link': m_aluminio + m_polea , 'com_link': [0.1, 0, 0], 't_rated': 1.0},
+        {'axis': 'y', 'offset': [0.2, 0, 0],       'm_servo': 10/1000,         'm_link': m_aluminio, 'com_link': [0.1, 0, 0], 't_rated': 1.0},
+    ]
+
+    # Inicializamos con una aceleración que VAMOS A PROBAR (ej: 15 rad/s^2)
+    arm_6dof = r3d(joints=robot_def, alpha_target=5.0, m_load=m_load)
+
+    # Posición horizontal extrema
+    arm_6dof.analyze_pose([0, 0, 0, 0, 0, 0])
+
+
+def caso2gof():
+    m_5840_31zy = 360/1000
+    m_jgy370 = 180/1000
+
+    m_gripper = 50/1000   
+    m_load = 20 /1000 
+
+    robot_def = [
+        {'axis': 'z', 'offset': [0.15, 0, 0],       'm_servo': m_jgy370, 'm_link': 0.1, 'com_link': [0, 0, 0.05], 't_rated':1.0 },
+        {'axis': 'y', 'offset': [0.2, 0, 0],    'm_servo': m_jgy370, 'm_link': 70/1000, 'com_link': [0.1, 0, 0], 't_rated': 1.0},
+        {'axis': 'y', 'offset': [0.2, 0, 0],       'm_servo': m_jgy370,         'm_link': 70/1000, 'com_link': [0.1, 0, 0], 't_rated': 1.0},
+    ]
+
+    # Inicializamos con una aceleración que VAMOS A PROBAR (ej: 15 rad/s^2)
+    arm_6dof = r3d(joints=robot_def, alpha_target=5.0, m_load=m_load)
+
+    # Posición horizontal extrema
+    arm_6dof.analyze_pose([0, 0, 0, 0, 0, 0])
 
 
 
@@ -111,8 +154,10 @@ def caso4_pol_gof():
 
 
 
-caso6gof()
-caso6_pol_gof()
+#caso6gof()
+#caso6_pol_gof()
 #caso4gof()
 #caso4_pol_gof()
+caso2_pol_gof()
+
 
